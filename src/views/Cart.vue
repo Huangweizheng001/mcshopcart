8a05190187404362df4e0ad355f418a547cecf52
huangweizheng <378507327@qq.com>
2018-08-31 11:33:26 +0800

    update
    update

---
+++ src/views/Cart.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    这是购物车页面
+    <span>{{$route.query.goodsId}}</span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {}
+</script>
+
+<style scoped lang="less" rel="stylesheet/less">
+
+</style>
